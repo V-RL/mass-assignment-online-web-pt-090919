822ab68c4f86f8955dcfb99aa1747e39ea2d737a
Brandon Furr <brandon.furr123@gmail.com>
2019-10-21 23:17:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_accessor :name, :birthday, :hair_color
   
-  def initialize(attributes)
+  def initialize(attributes:)
     attributes.each {|k, v| self.send (("#{key}= "), value)}
   end
 end
